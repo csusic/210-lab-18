41e786f44b50ecacb752a6cf59e7001bed604c1c
Christine Susic <csusic022@insite.4cd.edu>
2026-03-07 14:40:33 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,37 @@
+// COMSC-210 | Lab 18 | Christine Susic
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const int W1 = 8;
+
+struct Node {
+    int rating;
+    string comment;
+    Node *next;
+};
+
+int main() {
+    int choice;
+    
+    cout << "Which linked list method should we use?" << endl;
+    cout << "[1] New nodes are added at the head of the linked list" << endl;
+    cout << "[2] New nodes are added at the tail of the linked list" << endl;
+    cin >> choice;
+    
+    cout << "Enter review rating 0-5: " << endl;
+    
+    //new nodes at head 
+    
+    //new nodes at tail
+    
+    cin >> rating;
+    cin >> comments;
+    
+    //traverse linked list
+  
+    //output data
+    
+    return 0;
+}
